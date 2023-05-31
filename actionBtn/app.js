4c837d53907c3a2335a652714f862bce2c9f7ef2
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-05-31 20:22:33 +0900

    add logic to toggle the action btn

--- actionBtn/app.js
+++ actionBtn/app.js
@@ -5,3 +5,24 @@ const profile = document.querySelector('.profile');
 const community = document.querySelector('.community');
 const explore = document.querySelector('.explore');
 const friends = document.querySelector('.friends');
+
+function action() {
+    if (trigger.classList.contains('clicked')) {
+        trigger.classList.remove('clicked');
+        home.classList.remove('home-animation');
+        profile.classList.remove('profile-animation');
+        community.classList.remove('community-animation');
+        explore.classList.remove('explore-animation');
+        friends.classList.remove('friends-animation');
+    } else {
+        trigger.classList.add('clicked');
+        home.classList.add('home-animation');
+        profile.classList.add('profile-animation');
+        community.classList.add('community-animation');
+        explore.classList.add('explore-animation');
+        friends.classList.add('friends-animation');
+    }
+}
+
+trigger.addEventListener('click', action);
+
