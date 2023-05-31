22956cafa26e4763938f45f8d2bfd77f83944a81
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-05-31 19:38:18 +0900

    grab the nodes

---
+++ actionBtn/app.js
@@ -0,0 +1,7 @@
+const trigger = document.querySelector('.trigger');
+const menu = document.querySelector('.menu');
+const home = document.querySelector('.home');
+const profile = document.querySelector('.profile');
+const community = document.querySelector('.community');
+const explore = document.querySelector('.explore');
+const friends = document.querySelector('.friends');
