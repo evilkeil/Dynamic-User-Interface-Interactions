d2477fa0018be3f9ba408b434302dfe517567a1a
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-06-02 04:27:53 +0900

    do animations for profile

--- actionBtn/test.js
+++ actionBtn/test.js
@@ -1,15 +1,20 @@
 const trigger = document.querySelector('.trigger');
 const home = document.querySelector('.home');
+const profile = document.querySelector('.profile');
 
 function action() {
     if (trigger.classList.contains('clicked')) {
         trigger.classList.remove('clicked');
         home.classList.remove('home-grow');
         home.classList.add('home-shrink');
+        profile.classList.remove('profile-grow');
+        profile.classList.add('profile-shrink');
     } else {
         trigger.classList.add('clicked');
         home.classList.add('home-grow');
         home.classList.remove('home-shrink');
+        profile.classList.add('profile-grow');
+        profile.classList.remove('profile-shrink');
     }
 }
 
