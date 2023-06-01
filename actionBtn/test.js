462298c554407cbf18d63f76a7eb8c12d31d0343
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-06-02 04:35:29 +0900

    add logic to make community toggled

--- actionBtn/test.js
+++ actionBtn/test.js
@@ -1,6 +1,7 @@
 const trigger = document.querySelector('.trigger');
 const home = document.querySelector('.home');
 const profile = document.querySelector('.profile');
+const community = document.querySelector('.community');
 
 function action() {
     if (trigger.classList.contains('clicked')) {
@@ -9,12 +10,16 @@ function action() {
         home.classList.add('home-shrink');
         profile.classList.remove('profile-grow');
         profile.classList.add('profile-shrink');
+        community.classList.remove('community-grow');
+        community.classList.add('community-shrink');
     } else {
         trigger.classList.add('clicked');
         home.classList.add('home-grow');
         home.classList.remove('home-shrink');
         profile.classList.add('profile-grow');
         profile.classList.remove('profile-shrink');
+        community.classList.add('community-grow');
+        community.classList.remove('community-shrink');
     }
 }
 
