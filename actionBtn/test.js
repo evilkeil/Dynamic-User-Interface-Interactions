ba32644da89db7619864a3364c69821a75254d32
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-06-03 04:09:32 +0900

    add logic to the explore animation toggle

--- actionBtn/test.js
+++ actionBtn/test.js
@@ -2,6 +2,7 @@ const trigger = document.querySelector('.trigger');
 const home = document.querySelector('.home');
 const profile = document.querySelector('.profile');
 const community = document.querySelector('.community');
+const explore = document.querySelector('.explore');
 
 function action() {
     if (trigger.classList.contains('clicked')) {
@@ -12,6 +13,8 @@ function action() {
         profile.classList.add('profile-shrink');
         community.classList.remove('community-grow');
         community.classList.add('community-shrink');
+        explore.classList.remove('explore-grow');
+        explore.classList.add('explore-shrink');
     } else {
         trigger.classList.add('clicked');
         home.classList.add('home-grow');
@@ -20,6 +23,8 @@ function action() {
         profile.classList.remove('profile-shrink');
         community.classList.add('community-grow');
         community.classList.remove('community-shrink');
+        explore.classList.add('explore-grow');
+        explore.classList.remove('explore-shrink');
     }
 }
 
