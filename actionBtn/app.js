fefe5fa09fdf0e40d27f494388c161f6247aa1d4
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-06-03 04:18:01 +0900

    move test.js to app.js

--- actionBtn/app.js
+++ actionBtn/app.js
@@ -1,44 +1,36 @@
 const trigger = document.querySelector('.trigger');
-const menu = document.querySelector('.menu');
 const home = document.querySelector('.home');
 const profile = document.querySelector('.profile');
 const community = document.querySelector('.community');
 const explore = document.querySelector('.explore');
 const friends = document.querySelector('.friends');
 
-// function action() {
-//     if (trigger.classList.contains('clicked')) {
-//         trigger.classList.remove('clicked');
-//         home.classList.remove('home-grow');
-//         home.classList.add('home-shrink');
-//         profile.classList.remove('profile-animation');
-//         community.classList.remove('community-animation');
-//         explore.classList.remove('explore-animation');
-//         friends.classList.remove('friends-animation');
-//     } else {
-//         trigger.classList.add('clicked');
-//         home.classList.add('home-grow');
-//         home.classList.remove('home-shrink');
-//         profile.classList.add('profile-animation');
-//         community.classList.add('community-animation');
-//         explore.classList.add('explore-animation');
-//         friends.classList.add('friends-animation');
-//     }
-// }
-
-
 function action() {
     if (trigger.classList.contains('clicked')) {
         trigger.classList.remove('clicked');
         home.classList.remove('home-grow');
         home.classList.add('home-shrink');
+        profile.classList.remove('profile-grow');
+        profile.classList.add('profile-shrink');
+        community.classList.remove('community-grow');
+        community.classList.add('community-shrink');
+        explore.classList.remove('explore-grow');
+        explore.classList.add('explore-shrink');
+        friends.classList.remove('friends-grow');
+        friends.classList.add('friends-shrink');
     } else {
         trigger.classList.add('clicked');
         home.classList.add('home-grow');
         home.classList.remove('home-shrink');
+        profile.classList.add('profile-grow');
+        profile.classList.remove('profile-shrink');
+        community.classList.add('community-grow');
+        community.classList.remove('community-shrink');
+        explore.classList.add('explore-grow');
+        explore.classList.remove('explore-shrink');
+        friends.classList.add('friends-grow');
+        friends.classList.remove('friends-shrink');
     }
 }
 
-
 trigger.addEventListener('click', action);
-
